af39fb13797c038b4d7bd1979990aeedd930d409
Użytkownik <uytkownik@kursGIT.pl>
2020-07-19 13:50:48 +0200

    zad 15 rozwiązane

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// Napisz  program,  który  pobierze  od  użytkownika  10  dowolnie  dużych  liczb (zmiennych typu int) i wypisze te,
+        // Napisz  program,  który  pobierze  od  użytkownika  10  dowolnie  dużych  liczb (zmiennych typu int) i wypisze te,
         // które wystąpiły minimum dwukrotnie.
 
-
-
+        Zadanie15 zadanie15 = new Zadanie15();
+        Number[] numbers = zadanie15.getNumbers();
+        zadanie15.printNumbers(numbers);
     }
 }
--- src/Zadanie15.java
+++ src/Zadanie15.java
@@ -1,3 +1,32 @@
-public class Zadanie15 {
+import java.util.Scanner;
 
+public class Zadanie15 {
+    private Scanner scanner = new Scanner(System.in);
+
+    public Number[] getNumbers() {
+        Number[] numbers = new Number[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Podaj 10 liczb: ");
+            int value = scanner.nextInt();
+            boolean find = false;
+            for (Number number : numbers) {
+                if (number != null && number.getValue() == value) {
+                    number.setContent(number.getValue() + 1);
+                    find = true;
+                }
+            }
+            if (!find) {
+                numbers[i] = new Number(value, 1);
+            }
+        }
+        return numbers;
+    }
+
+    public void printNumbers(Number[] numbers) {
+        for (Number number : numbers) {
+            if (number != null && number.getContent() > 1) {
+                System.out.println(number.getValue());
+            }
+        }
+    }
 }
